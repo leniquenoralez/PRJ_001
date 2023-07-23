3fda340392e656175a95855cba6444f0d6932ad5
Lenique Noralez <noralezl@wwt.com>
2023-07-23 11:59:11 -0500

    fix bugs and errors

--- C/command.c
+++ C/command.c
@@ -2,16 +2,29 @@
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <paths.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/wait.h>
 
 int command(const char *string, char *outbuf, int outlen, char *errbuf, int errlen)
 {
-    pid_t cmdpid;
+    pid_t pid;
 
+    int status, pstat;
     int outPipefd[2];
     int errPipefd[2];
 
     int outPipe = pipe(outPipefd);
     int errPipe = pipe(errPipefd);
+
+    sigset_t mask, omask;
+    struct sigaction sa = {
+        .sa_handler = SIG_IGN,
+        .sa_flags = 0,
+        .sa_mask = 0,
+    };
+
 
     if (outPipe == -1 || errPipe == -1)
     {
@@ -19,16 +32,16 @@ int command(const char *string, char *outbuf, int outlen, char *errbuf, int errl
         exit(EXIT_FAILURE);
     }
 
-    cmdpid = fork();
-    if (cmdpid < 0)
+    pid = fork();
+    if (pid < 0)
     {
         perror("fork");
         return -1;
     }
 
-    if (cmdpid == 0)
+    if (pid == 0)
     {
-
+        printf("running: %s\n", string);
         close(STDOUT_FILENO);
         dup(outPipefd[1]);
 
@@ -38,22 +51,27 @@ int command(const char *string, char *outbuf, int outlen, char *errbuf, int errl
         close(outPipefd[0]);
         close(errPipefd[0]);
 
-        execlp("/bin/sh","sh", "-c", string, NULL);
-
-        return 0;
-    } else {
-
-        if (waitpid(cmdpid, NULL, 0) < 0)
-        {
-            perror("waitpid error");
-            return -1;
-        }
-
-        read(outPipefd[0], outbuf, outlen);
-        return 0;
+        execlp(_PATH_BSHELL, "sh", "-c", string, NULL);
+        _exit(127);
     }
+    close(STDIN_FILENO);
 
+    sigemptyset(&sa.sa_mask);
+    dup(outPipefd[0]);
+    close(outPipefd[1]);
+    close(errPipefd[1]);
 
+    sigemptyset(&mask);
 
-    return 0;
+    sigaddset(&mask, SIGCHLD);
+    sigprocmask(SIG_BLOCK, &mask, &omask);
+
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGQUIT, &sa, NULL);
+
+    waitpid(pid, &pstat, 0);
+
+    read(outPipefd[0], outbuf, outlen);
+    read(errPipefd[0], errbuf, errlen);
+    return (pid == -1 ? pid : pstat);
 }
--- C/main.c
+++ C/main.c
@@ -7,10 +7,20 @@ int main(int argc, char const *argv[])
     const char *string = argv[1];
     char outbuf[BUFSIZ];
     char errbuf[BUFSIZ];
-    if (command("ls -l", outbuf, BUFSIZ - 1, errbuf, BUFSIZ - 1) == -1)
+
+
+    if (!string)
     {
+        printf("Usage: ./command [command]");
+        return EXIT_FAILURE;
+    }
+
+
+    if (command(string, outbuf, BUFSIZ - 1, errbuf, BUFSIZ - 1) == -1)
+    {
+        printf("EXIT_FAILURE");
         perror("command");
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
     outbuf[BUFSIZ - 1] = '\0';
     errbuf[BUFSIZ - 1] = '\0';
